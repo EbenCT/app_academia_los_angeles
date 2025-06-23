062c4e84301992343094f2a4df1d6975585cd0a4
EbenCT <cayoeben64@gmail.com>
2025-06-23 12:42:08 -0400

    agregados terminos y condiciones

--- lib/constants/api_constanst.dart
+++ lib/constants/api_constanst.dart
@@ -1,7 +1,7 @@
 /// Constantes para las llamadas a la API
 class ApiConstants {
   /// URL base de la API
-  static const String baseUrl = 'http://192.168.100.4:3000/graphql'; //'https://academia-leveling-backend.onrender.com/graphql';//'https://academialevelingbackend-production.up.railway.app/graphql';
+  static const String baseUrl = 'http://10.29.10.49:3000/graphql'; //'https://academia-leveling-backend.onrender.com/graphql';//'https://academialevelingbackend-production.up.railway.app/graphql';
   /// Constantes de tiempo
   static const int apiTimeoutSeconds = 30;
 }
